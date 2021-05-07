ec3b10c90dbcf094ceab3311e1f33835781518b1
cristiano-saraiva <cristiano.saraivamello@outlook.com>
2021-05-06 22:23:02 -0300

    Configuração de templates

--- vendor/hcodebr/php-classes/src/index.php
+++ vendor/hcodebr/php-classes/src/index.php
@@ -1,19 +1,19 @@
-<?php
+<?php 
 
 require_once("vendor/autoload.php");
 
-use \Slim\Slim;
-use \Hcode\Page;
-
-$app = new Slim();
+$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function () {
-
-	$page = new Page();
+$app->get('/', function() {
+    
+	$page = new Hcode\Page();
 
 	$page->setTpl("index");
+
 });
 
 $app->run();
+
+ ?>
